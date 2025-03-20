40a14726b53aa4ab19c98a9422bac6b72a30e086
tsukagoshi-ren <95907986+tsukagoshi-ren@users.noreply.github.com>
2025-03-20 21:34:12 +0900

    Remove unnecessary files

--- workshop-webapp/src/main/java/com/reach/shared/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.reach.shared.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.reach.shared.models.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
